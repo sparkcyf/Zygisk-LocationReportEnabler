2283861887a7f4422aabfaa54e6da32488555cbe
lelemka0 <20322043+lelemka0@users.noreply.github.com>
2021-09-17 03:00:51 +0800

    v1.1.4

--- template/magisk_module/create_config.sh
+++ template/magisk_module/create_config.sh
@@ -8,16 +8,24 @@ echo -n "CN" >"$MODPATH/config/properties/ro.product.locale.region"
 echo -n "cn" >"$MODPATH/config/properties/ro.product.locale.language"
 
 mkdir -p "$MODPATH/config/packages"
-#contacts && mms
+#contacts && mms (黄页、短信分类、ai通话)
 touch "$MODPATH/config/packages/com.android.contacts"
 touch "$MODPATH/config/packages/com.android.mms"
 touch "$MODPATH/config/packages/com.xiaomi.aiasst.service"
+touch "$MODPATH/config/packages/com.xiaomi.aiasst.vision"
 touch "$MODPATH/config/packages/com.miui.yellowpage"
-
+#日历
 touch "$MODPATH/config/packages/com.android.calendar"
-touch "$MODPATH/config/packages/com.miui.virtualsim"
+#通知中心
 touch "$MODPATH/config/packages/com.miui.notification"
+#翻译服务
 touch "$MODPATH/config/packages/com.miui.translationservice"
+touch "$MODPATH/config/packages/com.miui.translation.kingsoft"
+touch "$MODPATH/config/packages/com.miui.translation.youdao"
+#传送门
 touch "$MODPATH/config/packages/com.miui.contentextension"
+#负一屏
 touch "$MODPATH/config/packages/com.miui.personalassistant"
+#必要服务
 touch "$MODPATH/config/packages/com.miui.contentcatcher"
+touch "$MODPATH/config/packages/com.miui.cloudservice.sysbase"
